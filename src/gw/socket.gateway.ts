a35038ccccb6f6dfeb36702c6cb1f4b8246d7106
oleg shchepotkin <oleg.shcepotkin@leroymerlin.ru>
2024-04-11 17:13:20 +0300

    (init): init setup

---
+++ src/gw/socket.gateway.ts
@@ -0,0 +1,25 @@
+import { SubscribeMessage, WebSocketGateway, WebSocketServer, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway()
+export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+
+  afterInit(server: Server) {
+    console.log('Socket initialized');
+  }
+
+  handleConnection(client: any, ...args: any[]) {
+    console.log('Client connected:', client.id);
+  }
+
+  handleDisconnect(client: any) {
+    console.log('Client disconnected:', client.id);
+  }
+
+  @SubscribeMessage('notify_server')
+  handleMessage(client: any, payload: string): void {
+    console.log('Received message:', payload);
+    this.server.emit('notify_client', payload);
+  }
+}
